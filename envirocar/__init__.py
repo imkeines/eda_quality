7dec21222531185cd2bbe8f48c4c63d86d5c3f52
imkeines <i.klatt@mailbox.org>
2020-10-21 15:17:20 +0200

    delete track_generalization

--- envirocar/__init__.py
+++ envirocar/__init__.py
@@ -17,5 +17,3 @@ from .EDA import correction
 from .trajectories.preprocessing import Preprocessing
 from .trajectories.track_converter import TrackConverter
 from .trajectories.visualisation import Visualiser
-from .trajectories.track_similarity import *
-
